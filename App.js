57c0987d01ea320a5c136a5b0a0dd7f9f4d127e4
Paradox369 <george.ketcha@gmail.com>
2023-03-19 16:34:59 +0100

    flexbox basics and icons

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
   FlatList,
   Alert,
@@ -68,8 +67,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
+    flex: 1,
   },
   list: {
     marginTop: 20,
+    flex: 1,
   },
 });
